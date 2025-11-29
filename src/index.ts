e9c952746c54aeaa79b11447a738bae7d837eb00
Dawda-2m-Corp <dkujabi@2m-corp.com>
2025-11-29 17:57:05 +0000

    Starting from scratch because I let ai do some shit :(

--- src/index.ts
+++ src/index.ts
@@ -11,49 +11,12 @@
  */
 
 // Core exports
-export * from './types/primitives';
-export * from './validation/schemas';
 
-// Base classes
-export { FhirResource, DomainResource } from './r4/baseResource';
 
-// Data types
-export { Meta } from './r4/meta';
-export { Period } from './r4/dataTypes/period';
-export { Coding } from './r4/dataTypes/coding';
-export { CodeableConcept } from './r4/dataTypes/codeableConcept';
-export { Identifier } from './r4/identifier';
-export { Reference } from './r4/reference';
-
-// Resources
-export { Account, AccountCoverage, AccountGuarantor } from './r4/account';
-
-// Factory functions
-export {
-    constructFhirElement,
-    createFhirResource,
-    isSupportedResourceType,
-    getSupportedResourceTypes
-} from './r4/factory';
-
-// Utility functions
-export {
-    convertFhirResourceObjectToJson,
-    validateFhirResource,
-    cloneFhirResource,
-    compareFhirResources,
-    extractReferences,
-    getResourceVersion,
-    getLastUpdated
-} from './utils';
-
-// Version and library info
 export const VERSION = '2.0.0';
 export const FHIR_VERSION = 'R4';
 
-/**
- * Library information
- */
+
 export const LIBRARY_INFO = {
     name: 'fhir-resources-ts',
     version: VERSION,
--- src/r4/activityDefinition.ts
+++
@@ -1,5 +0,0 @@
-import { DomainResource } from "./baseResource";
-
-export class ActivityDefinition extends DomainResource {
-    resourceType: 'ActivityDefinition' = 'ActivityDefinition';
-}
