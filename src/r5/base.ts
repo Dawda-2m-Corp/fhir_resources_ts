8cf37b8d1f78e0b7025d1d07f005c276f8d1d190
Dawda-2m-Corp <dkujabi@2m-corp.com>
2025-12-09 14:34:20 +0000

    Updating...

--- src/r5/base.ts
+++ src/r5/base.ts
@@ -1,6 +1,4 @@
 import { BaseFhirResource as BaseFhirResourceR4 } from "../r4";
-import { Extension } from "./extension";
 
 export class BaseFhirResource extends BaseFhirResourceR4 {
-    extension?: Extension[] | undefined;
 }
